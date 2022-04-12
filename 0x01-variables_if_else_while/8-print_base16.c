074e8a2a68d9105a86c4f557a46c95843af16246
Ginkal <ginikaolekamma@gmail.com>
2022-04-12 02:30:04 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 
@@ -10,7 +10,8 @@ int main(void)
 char base_16;
 
 for (base_16 = 48; base_16 <= 57; base_16++)
-{putchar(base_16);
+{
+putchar(base_16);
 }
 putchar ('\n');
 return (0);
